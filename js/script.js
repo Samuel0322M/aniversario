620afa4bf0042019ffdc2a0d74f75f2a04a95480
Samuel0322M <marinsanchezs040@gmail.com>
2025-05-07 19:52:09 -0500

    se cambia imagenes y se agrega javascript

--- js/script.js
+++ js/script.js
@@ -1,61 +1,256 @@
-var tarjeta = document.querySelectorAll('.tarjeta');
+    const datosTarjeta = [
+        {
+            imagen: "/aniversario/images/semanaantesdenovios.webp",
+            titulo: "Antes del comienzo",
+            texto:
+                "Recuerdo este dia hoy con cariño, fue el dia que marco el antes y el despues en nuestra relacion, salida la cual hoy agradezco que haya sucedido porque significo que hoy estamos juntos.",
+        },
+        {
+            imagen: "/aniversario/images/noviosalfin.webp",
+            titulo: "22 de junio",
+            texto:
+                "22 de junio fue el gran dia, tan grande fue que ni una foto juntos, solo encontre eso como evidencia, recuerdo que ese dia no me lo creia y tarde varios dias en asimilar que te volviste mi novia aunque estaba feliz eso es innegable.",
+        },
+        {
+            imagen: "/aniversario/images/semanadespues.webp",
+            titulo: "Julio 6",
+            texto:
+                "Fue nuestra primer cita oficial, recuerdo haber ido a tu casa y presentarme como tu novio bastante nervioso las cosas como son pero estaba contento de poderme mostrar a tu familia como tu novio y apenas empezaba a asimilar que ya somos novios.",
+        },
+        {
+            imagen: "/aniversario/images/almuerzojuntos.webp",
+            titulo: "Salida de la u",
+            texto:
+                "Recuerdo este dia cuando fui a recogerte en la u lo cual se sentia un poco extraño, almorzaste el arroz mixto y conoci un poco mas de ti y de yumbo ya que estaba muy nervioso para estar en tu casa.",
+        },
+        {
+            imagen: "/aniversario/images/cocinandote.webp",
+            titulo: "Cita en mi casa",
+            texto:
+                "fue por los dias donde mi madre estaba con mi hermana los cuales aprovechamos muy bien, fue de las primeras veces que cocinaba algo mas alla de algo normal y encima para alguien mas, pero mas alla de eso fue una compañia en dias solitarios.",
+        },
+        {
+            imagen: "/aniversario/images/juntos7w7.webp",
+            titulo: "First time",
+            texto:
+                "Bueno, fue una experiencia, fue bonito fue novedoso pero aun mas importante fue especial porque fue con alguien que quiero mucho y hoy en dia mas guardo ese dia en mi mente y lo atesoro mucho porque tambien fue un gran paso en la relacion",
+        },
+        {
+            imagen: "/aniversario/images/manilla.webp",
+            titulo: "Nuestro Lazo",
+            texto:
+                "Fue uno de nuestros primeros regalos aunque fuiste tu quien lo compro realmente aun la uso y la guardo tiene un algo que no se que es que me da paz y me recuerda a ti.",
+        },
+        {
+            imagen: "/aniversario/images/11septiembre.webp",
+            titulo: "Tu cumpleaños 11 septiembre",
+            texto:
+                "Bueno, tu primer cumpleaños juntos recuerdo que desde 2 o 3 meses tenia en mente el cuarzo rosa y aunque no pude estar en tu dia como tal especial espero con ansias este e intentar poder acompañarte ese dia.",
+        },
+        {
+            imagen: "/aniversario/images/halloween.webp",
+            titulo: "Me la conviertes en piedra",
+            texto:
+                "Hallowen, fue un dia que no estuvimos juntos pero estabas hermosa, aun recuerdo el dia me hubiese gustado estar contigo y poder acompañarte fue un dia que me dejo reflexionando y de querer estar mas contigo en los dias de celebraciones.",
+        },
+        {
+            imagen: "/aniversario/images/26nov.webp",
+            titulo: "En mitad de fiestas",
+            texto:
+                "No es hallowen ni navidad es noviembre que llego este dia es uno de mis favoritos siento que hicimos muchs cosas ese dia, que nos acercamos mas y nos divertimos mucho juntos que ya empezabamos a estar mas relajados el uno con el otro.",
+        },
+        {
+            imagen: "/aniversario/images/22dic.webp",
+            titulo: "Navidad y mi cumpleaños",
+            texto:
+                "Este fue un dia raro las cosas como son, ya que habia aceptado que era un dia mas que no era importante y oh que equivocado estaba tu cambiaste eso y la verdad ahora pienso en el proximo cumpleaños en hacer algo que se sienta especial junto a ti.",
+        },
+        {
+            imagen: "/aniversario/images/29dic.webp",
+            titulo: "SONICCCCCC",
+            texto:
+                "29 de diciembre me acompañaste a ver sonic 3 uno de mias dias favoritos los cuales atesorare mucho ya que me hizo muy feliz, tambien fue el dia que se te daño la cadena y como no tomamos fotos esta es mi forma de recordarlo",
+        },
+        {
+            imagen: "/aniversario/images/9feb-2.webp",
+            titulo: "9 de febrero",
+            texto:
+                "Fue una salida tranquila, almorzamos juntos estuvimos en el unico hablamos nos reimos nos dimos mimos fue una salida muy bonita y me encanto ese tiempo juntos.",
+        },
+        {
+            imagen: "/aniversario/images/30abril.webp",
+            titulo: "29 de marzo",
+            texto:
+                "Fue el dia que fuimos aurgencia con mi madre fue un recuerdo amargo pero estar contigo lo hizo muy especial ya que estuviste paara mi cuando mas lo necesite y hasta el dia de hoy te agradezco que me hayas acompañado eres una chica maravillosa.",
+        },
+        {
+            imagen: "/aniversario/images/5abril1.webp",
+            titulo: "5 de abril",
+            texto:
+                "Una semana despues de lo sucedido, estabas bastante ocupada con tus trabajos y estesada y aun asi viniste a veme, no niego que me senti mal peo tambien muy feliz que me hayas acompañado",
+        },
+        {
+            imagen: "/aniversario/images/13abil1.webp",
+            titulo: "Tu Primer Te Amo",
+            texto:
+                "Estuvimos juntos el fin de semana y fue el dia 11 que me dijiste tu primer te amo me compraste helado y fuiste muy bonita conmigo, como empezo semana santa pues me fuiste a ver el domingo e hicimos lo que no hicimos el sabado 7w7",
+        },
+        {
+            imagen: "/aniversario/images/18abril4.webp",
+            titulo: "Space Jump",
+            texto:
+                "Tuvimos una salida muy bonita que hicimos de todo, ejercicio, nos reimos tomamos algo, y fue el 18 de abril que te dije mi pime te amo, ya que fue el dia que llore frente a ti.",
+        },
+        {
+            imagen: "/aniversario/images/20abril2.webp",
+            titulo: "Piscinaaa",
+            texto:
+                "Aprovechamos la semana santa y fuimos a piscina, nos divertimos mucho estuvimos juntos todo el dia tenia muchas ganas de ir y de verte en traje de baño y me encanto es uno de mis paseos favoritos.",
+        },
+        {
+            imagen: "/aniversario/images/mayo4.webp",
+            titulo: "Mayo 4",
+            texto:
+                "Fue una semana y fin de semana que pasaron cosas, pues el dia jueves hice la promesa en cambiar y mejorar quedarme callado y lastimarte y si quiero que esta pagina sea momentos felices y bonitos pero para cuando veas esto quiero que mires hacia a tras y veas que si lo cambie.",
+        },
+        {
+            imagen: "/aniversario/images/mayo4-2.webp",
+            titulo: "Mayo 4",
+            texto:
+                "Me diverti mucho el sabado fue muy bonito y divertido viendo fuck news y comiendo juntos, cada dia me haces quererte mas y que seas tu, el domingo nos vimos tambien y estuvimos bien bailar a la gente y me mataste de un susto yendo en moto.",
+        },
+    ];
 
-tarjeta.forEach(tarjeta => {
-    tarjeta.addEventListener("click", () => { 
-        tarjeta.classList.toggle("girada");
-    });
+    const rutasImagenes = [
+        "/aniversario/images/6julio1.webp",
+        "/aniversario/images/26nov.webp",
+        "/aniversario/images/citaencasa2.webp",
+        "/aniversario/images/paraguas2.webp",
+        "/aniversario/images/mayo4.webp",
+        "/aniversario/images/9feb-1.webp",
+        "/aniversario/images/semanaantes2.webp",
+        "/aniversario/images/20abril1.webp",
+        "/aniversario/images/18abril1.webp",
+        "/aniversario/images/5abril1.webp",
+        "/aniversario/images/Imagen de WhatsApp 2025-04-03 a las 20.07.44_a4e6a7ed.webp",
+        "/aniversario/images/6julio.webp",
+        "/aniversario/images/videollamad1.webp",
+        "/aniversario/images/conmisaco.webp",
+        "/aniversario/images/18abril.webp",
+        "/aniversario/images/13abil.webp",
+        "/aniversario/images/cocinandote.webp",
+        "/aniversario/images/5abril2.webp",
+        "/aniversario/images/mayo4-2.webp",
+        "/aniversario/images/manilla.webp",
+        "/aniversario/images/mia.webp",
+        "/aniversario/images/20abril2.webp",
+        "/aniversario/images/almuerzojuntos.webp",
+        "/aniversario/images/gibi2.webp",
+        "/aniversario/images/13abil1.webp",
+        "/aniversario/images/18abril2.webp",
+        "/aniversario/images/20abril3.webp",
+        "/aniversario/images/citaencasa.webp",
+        "/aniversario/images/5abril3.webp",
+        "/aniversario/images/paraguas1.webp",
+        "/aniversario/images/22dic.webp",
+        "/aniversario/images/20abril4.webp",
+        "/aniversario/images/peliconmadre.webp",
+        "/aniversario/images/18abril3.webp",
+        "/aniversario/images/juntos7w7.webp",
+        "/aniversario/images/20abril5.webp",
+        "/aniversario/images/videollamada3.webp",
+        "/aniversario/images/mayo4-1.webp",
+        "/aniversario/images/despuesdeldeli.webp",
+        "/aniversario/images/paraguas4.webp",
+        "/aniversario/images/gibi1.webp",
+        "/aniversario/images/videollamad2.webp",
+        "/aniversario/images/18abril4.webp",
+        "/aniversario/images/20abril.webp"
+      ];
 
-    tarjeta.addEventListener("touchstart", () => { 
-        tarjeta.classList.toggle("girada");
-    });
+const contenedorCards = document.getElementById("contenedor-tarjetas");
+
+datosTarjeta.forEach(tarjeta => {
+        const div = document.createElement("div");
+        div.classList.add("tarjeta");
+        div.innerHTML = `
+          <div class="box-frente">
+            <img src="${tarjeta.imagen}" alt="">
+          </div>
+          <div class="box-detras">
+            <h5>${tarjeta.titulo}</h5>
+            <p>${tarjeta.texto}</p>
+          </div>
+        `;
+        contenedorCards.appendChild(div);
+      });
+
+const contenedorSwiper = document.getElementById("swiper-wrapper");
+
+rutasImagenes.forEach(ruta => {
+    const div = document.createElement("div");
+    div.classList.add("swiper-slide");
+    div.innerHTML = `<img src="${ruta}" alt="recuerdo">`;
+    contenedorSwiper.appendChild(div);
+  });
+
+var tarjeta = document.querySelectorAll(".tarjeta");
+
+tarjeta.forEach((tarjeta) => {
+  tarjeta.addEventListener("click", () => {
+    tarjeta.classList.toggle("girada");
+  });
+
+  tarjeta.addEventListener("touchstart", () => {
+    tarjeta.classList.toggle("girada");
+  });
 });
 
-
 var swiper = new Swiper(".mySwiper", {
-    loop: true,
-    autoplay: {
-        delay: 3000,
-        disableOnInteraction: false
-    },
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev"
-    },
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true
-    }
+  loop: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
 });
 
 function abrirSobre(elemento) {
-    const cerrado = elemento.querySelector('.cerrado');
-    const abierto = elemento.querySelector('.abierto');
-    const carta = elemento.querySelector('.carta');
+  const cerrado = elemento.querySelector(".cerrado");
+  const abierto = elemento.querySelector(".abierto");
+  const carta = elemento.querySelector(".carta");
 
-    cerrado.style.display = "none"; // Oculta el sobre cerrado
-    abierto.style.display = "block"; // Muestra el sobre abierto
+  cerrado.style.display = "none"; // Oculta el sobre cerrado
+  abierto.style.display = "block"; // Muestra el sobre abierto
 
-    setTimeout(() => {
-        carta.style.transform = "translateX(-50%) scale(1)"; // Hace que la carta aparezca
-        carta.style.opacity = "1";
-    }, 300);
+  setTimeout(() => {
+    carta.style.transform = "translateX(-50%) scale(1)"; // Hace que la carta aparezca
+    carta.style.opacity = "1";
+  }, 300);
 }
 
 function cerrarSobre(event, boton) {
-    event.stopPropagation(); // Evita que se active el evento de abrir el sobre
+  event.stopPropagation(); // Evita que se active el evento de abrir el sobre
 
-    const carta = boton.parentElement; // Obtiene la carta
-    const sobre = carta.closest('.sobre');
-    const cerrado = sobre.querySelector('.cerrado');
-    const abierto = sobre.querySelector('.abierto');
+  const carta = boton.parentElement; // Obtiene la carta
+  const sobre = carta.closest(".sobre");
+  const cerrado = sobre.querySelector(".cerrado");
+  const abierto = sobre.querySelector(".abierto");
 
-    carta.style.transform = "translateX(-50%) scale(0)"; // Oculta la carta
-    carta.style.opacity = "0";
+  carta.style.transform = "translateX(-50%) scale(0)"; // Oculta la carta
+  carta.style.opacity = "0";
 
-    setTimeout(() => {
-        abierto.style.display = "none"; // Oculta el sobre abierto
-        cerrado.style.display = "block"; // Muestra el sobre cerrado nuevamente
-    }, 500);
+  setTimeout(() => {
+    abierto.style.display = "none"; // Oculta el sobre abierto
+    cerrado.style.display = "block"; // Muestra el sobre cerrado nuevamente
+  }, 500);
 }
 
 const audio = document.getElementById("backgroundMusic");
@@ -63,19 +258,15 @@ const button = document.getElementById("toggleMusic");
 
 let isPlaying = false;
 
-
 function toggleMusic() {
-    if (!isPlaying) {
-        audio.play();
-        button.textContent = "Pausar música";
-      } else {
-        audio.pause();
-        button.textContent = "Reproducir música";
-      }
-      isPlaying = !isPlaying;
+  if (!isPlaying) {
+    audio.play();
+    button.textContent = "Pausar música";
+  } else {
+    audio.pause();
+    button.textContent = "Reproducir música";
+  }
+  isPlaying = !isPlaying;
 }
 
 button.addEventListener("click", toggleMusic);
-
-
-  
